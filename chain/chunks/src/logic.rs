978960dcabaf7e34441c43fa573c4d4319c3b650
Nikolay Kurtov <nikolay.kurtov@near.org>
2023-08-14 16:51:44 +0200

    test

--- chain/chunks/src/logic.rs
+++ chain/chunks/src/logic.rs
@@ -119,7 +119,7 @@ pub fn make_partial_encoded_chunk_from_owned_parts_and_needed_receipts<'a>(
         })
         .cloned()
         .collect();
-    let mut receipts: Vec<_> = receipts
+    let mut receipts: Vec<ReceiptProof> = receipts
         .filter(|receipt| {
             cares_about_shard
                 || need_receipt(prev_block_hash, receipt.1.to_shard_id, me, shard_tracker)
@@ -127,7 +127,11 @@ pub fn make_partial_encoded_chunk_from_owned_parts_and_needed_receipts<'a>(
         .cloned()
         .collect();
     // Make sure the receipts are in a deterministic order.
-    receipts.sort();
+    receipts.sort_by_key(
+        |ReceiptProof(_, ShardProof { from_shard_id, to_shard_id, proof: _proof })| {
+            (*from_shard_id, *to_shard_id)
+        },
+    );
     match header.clone() {
         ShardChunkHeader::V1(header) => {
             PartialEncodedChunk::V1(PartialEncodedChunkV1 { header, parts, receipts })
