0316e94f2c97af97b3a2b305fe106c4412b6f00d
Longarithm <the.aleksandr.logunov@gmail.com>
2023-10-31 13:35:05 +0400

    stamp - another iteration for seemingly correct solution

--- chain/client/src/client.rs
+++ chain/client/src/client.rs
@@ -790,12 +790,6 @@ impl Client {
                     Some(validator_signer) => Some(validator_signer.validator_id().clone()),
                     None => None,
                 };
-                // if block is genesis, there is no partial chunk
-                // let incoming_receipts = if prev_block.header().prev_hash() != &CryptoHash::default() {
-                //     self.chain.collect_incoming_receipts_from_block(&me, prev_block).unwrap()
-                // } else {
-                //     HashMap::from_iter([(shard_id, vec![])])
-                // };
                 let result = self.chain.apply_chunk_from_block_before_production(
                     &me,
                     prev_block,
